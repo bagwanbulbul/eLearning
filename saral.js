0d9c58c055171bef77998bab63206b5491952e4d
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-09-27 23:14:55 +0530

    creating exercises

--- saral.js
+++ saral.js
@@ -21,12 +21,14 @@ app.post('/post',(req,res)=>{
     fs.writeFileSync("data.json",JSON.stringify(Data,null,2))
     return res.json(Data)
 })
+// get all courses
 app.get('/get', function (req, res) {
     fs.readFile("data.json", 'utf8', function (err, data) {
        console.log( data );
        return res.end( data );
     });
  })
+//  selected courses
  app.get("/get/:name",function(req,res){
     let id = req.params.name
     var data = fs.readFileSync("data.json") 
@@ -38,6 +40,7 @@ app.get('/get', function (req, res) {
         }
     }
  });
+//  update data
  app.put('/put/:id',(req,res)=>{
 
     var id = req.params.id-1;
@@ -50,7 +53,28 @@ app.get('/get', function (req, res) {
     fs.writeFileSync("data.json", JSON.stringify(Data));
     console.log(Data)
     return res.json(Data)
-    
-    
- }),
+}),
+// post exercise data
+app.post('/postexercise',(req,res)=>{
+    var id = req.params.id;
+    var user1 ={
+        courseId:req.body.courseId,
+        name:req.body.name,
+        description:req.body.description,
+        content:req.body.content,
+        hint:req.body.hint
+
+    }
+    var data =fs.readFileSync("exercise.json")
+    console.log(data)
+    data = data.toString();
+    let Data = JSON.parse(data)
+    id=Data.length
+    user1["id"] = id+1;
+    console.log(user1);
+    Data.push(user1)
+    fs.writeFileSync("exercise.json",JSON.stringify(Data,null,2))
+    return res.json(Data)
+})
+
 app.listen(3000, () => console.log('server is listening'));
